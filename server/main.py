bc23ff0fd10e7ca53f21010968240289a77e5571
Mohammad Aman <102747651+AmanBits@users.noreply.github.com>
2026-03-05 12:34:49 +0530

    Add files via upload

---
+++ server/main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, UploadFile, File, Form
+import pdfplumber
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+API_URL = "https://openrouter.ai/api/v1/chat/completions"
+
+headers = {
+    "Authorization": "Bearer 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",
+    "Content-Type": "application/json"
+}
+
+
+@app.get("/")
+def home():
+    return {"message": "CV Matcher API Running"}
+
+
+@app.post("/match-cv")
+async def match_cv(
+    file: UploadFile = File(...),
+    job_description: str = Form(...)
+):
+
+    # Extract text from PDF
+    cv_text = ""
+
+    with pdfplumber.open(file.file) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                cv_text += text + "\n"
+
+    # Prompt for LLM
+    prompt = f"""
+You are an AI recruiter.
+
+Match the following CV with the job description and give:
+
+1. Match score (0-100)
+2. Matching skills
+3. Missing skills
+4. Short explanation
+
+CV:
+{cv_text}
+
+Job Description:
+{job_description}
+"""
+
+    data = {
+        "model": "meta-llama/llama-3-8b-instruct",
+        "messages": [
+            {"role": "user", "content": prompt}
+        ]
+    }
+
+    response = requests.post(API_URL, headers=headers, json=data)
+
+    result = response.json()
+    
+    ai_message = result["choices"][0]["message"]["content"]
+
+    return {
+        "filename": file.filename,
+        "analysis": ai_message
+    }
